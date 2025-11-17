54e8025e81298b24f73e9ce1f63f21599a3face5
Efstathios <modo575@hotmail.com>
2025-11-17 11:29:09 +0100

    add images from api

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
-
+async function fetcher(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
