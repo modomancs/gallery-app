5784bf5ea25427ad311019e7981277aab03818e3
Efstathios <modo575@hotmail.com>
2025-11-19 11:59:16 +0100

    add styling to nav

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,11 +1,32 @@
 import Link from "next/link";
+import styled from "styled-components";
+const NavStyle = styled.div`
+  display: inline-flex;
+  gap: 20px;
+  margin-bottom: 20px;
+  position: sticky;
+  bottom: 0;
+  background-color: #f9f9f9;
+  padding: 10px 0;
+  border-bottom: 5px solid #ccc;
+  z-index: 1000;
+  width: 13%;
+  align-items: center;
 
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+`;
 export default function Navigation() {
   return (
-    <div>
+    <NavStyle>
       <Link href="/">Gallery</Link>
       <Link href="/Spotlight">Spotlight</Link>
       <Link href="/favorites">Favorites</Link>
-    </div>
+    </NavStyle>
   );
 }
